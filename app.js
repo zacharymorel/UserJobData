0e35e259a05db58065b34b988afb630ebc13e01d
Zachary Morel <morelz133@gmail.com>
2017-07-25 17:45:04 -0400

    Project is done for the day.

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 const express = require('express')
-const mustache = require('mustache-express')
-
+const mustacheExpress = require('mustache-express')
+const userData = require('./data.js')
 const app = express()
+const userInfo = userData.users
+
+app.use(express.static('public'))
+app.engine('mustache', mustacheExpress())
+app.set('views', './templates')
+app.set('view engine', 'mustache')
+
+
+app.get('/', (request, response) => {
+  response.render('index', userData)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('My the Force be with you');
+})
